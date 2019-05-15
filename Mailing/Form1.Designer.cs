6eacd3e38a7bcb5d851124b9d8c6303d4fe7ae83
Daniela Omena <rdaniglad@hotmail.com>
2019-05-15 19:46:02 -0400

    2 Commit

--- Mailing/Form1.Designer.cs
+++ Mailing/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mailing
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbel3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtDisplayOutput = new System.Windows.Forms.TextBox();
+            this.btnDisplayOutput = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbl2
+            // 
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl2.Location = new System.Drawing.Point(46, 62);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(75, 20);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Address:";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbel3
+            // 
+            this.lbel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbel3.Location = new System.Drawing.Point(46, 95);
+            this.lbel3.Name = "lbel3";
+            this.lbel3.Size = new System.Drawing.Size(70, 20);
+            this.lbel3.TabIndex = 2;
+            this.lbel3.Text = "City:";
+            this.lbel3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(299, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "State:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(452, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Zip:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(164, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(473, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(164, 60);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(473, 20);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(164, 95);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(120, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(345, 95);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(101, 20);
+            this.txtState.TabIndex = 8;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(516, 96);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(121, 20);
+            this.txtZip.TabIndex = 9;
+            // 
+            // txtDisplayOutput
+            // 
+            this.txtDisplayOutput.Location = new System.Drawing.Point(47, 158);
+            this.txtDisplayOutput.Multiline = true;
+            this.txtDisplayOutput.Name = "txtDisplayOutput";
+            this.txtDisplayOutput.ReadOnly = true;
+            this.txtDisplayOutput.Size = new System.Drawing.Size(604, 178);
+            this.txtDisplayOutput.TabIndex = 10;
+            // 
+            // btnDisplayOutput
+            // 
+            this.btnDisplayOutput.Location = new System.Drawing.Point(47, 129);
+            this.btnDisplayOutput.Name = "btnDisplayOutput";
+            this.btnDisplayOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayOutput.TabIndex = 11;
+            this.btnDisplayOutput.Text = "&Display";
+            this.btnDisplayOutput.UseVisualStyleBackColor = true;
+            this.btnDisplayOutput.Click += new System.EventHandler(this.BtnDisplayOutput_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(563, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "E$xit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnDisplayOutput);
+            this.Controls.Add(this.txtDisplayOutput);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbel3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Mailing Label Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtDisplayOutput;
+        private System.Windows.Forms.Button btnDisplayOutput;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Mailing/Form1.cs
+++ Mailing/Form1.cs
@@ -10,11 +10,27 @@ using System.Windows.Forms;
 
 namespace Mailing
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDisplayOutput_Click(object sender, EventArgs e)
+        {
+            String buffer;
+            buffer = "Mailing Label: " + Environment.NewLine + Environment.NewLine;
+            buffer = buffer + "Name: " + txtName.Text + Environment.NewLine;
+            buffer = buffer + "Address: " + txtAddress.Text + Environment.NewLine;
+            buffer = buffer + "City: " + txtCity.Text +  "   State: " + txtState.Text +     "   Zip: " + txtZip.Text;
+            txtDisplayOutput.Text = buffer;
+           
         }
     }
 }
